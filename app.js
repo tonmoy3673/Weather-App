6be7080b4bb7d7f24636c60e3c9cda70dbcca0bf
tonmoy <tonmoy3673@gmail.com>
2025-03-01 15:36:00 +0600

    :truck: set api key

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log('connected')
+// ========= API Key =============//
+const apiKey='58e2668dce7ceb8bbb5f2f6714cb2de7';
+const apiUrl='https://api.openweathermap.org/data/2.5/weather?units=metric&q=dhaka';
